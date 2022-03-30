19708ed69a3e5fdff40d37f0bd4346927d287844
Nadine Großkreuz <nadine.grosskreuz@web.de>
2022-03-30 19:15:08 +0200

    Add javascript

---
+++ index.js
@@ -0,0 +1,6 @@
+const bookmark = document.querySelector ('[data-js="bookmark"]')
+const bookmarkActive = document.querySelector ('[data-js="bookmarkActive"]')
+
+bookmark.addEventListener('click'; () => {
+    bookmark = bookmarkActive;
+}
