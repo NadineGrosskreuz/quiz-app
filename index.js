a9333b58add94a3f13736595ac08c40dd0553c6b
Nadine Großkreuz <nadine.grosskreuz@web.de>
2022-03-30 21:57:41 +0200

    Add homework

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 const bookmark = document.querySelector ('[data-js="bookmark"]')
 const bookmarkActive = document.querySelector ('[data-js="bookmarkActive"]')
 
-bookmark.addEventListener('click'; () => {
-    bookmark = bookmarkActive;
-}
+bookmark.addEventListener("click", () => {
+    bookmark.classList.toggle("hidden");
+    bookmarkActive.classList.toggle("hidden");
+  });
+
+  bookmarkActive.addEventListener("click", () => {
+    bookmarkActive.classList.toggle("hidden");
+    bookmark.classList.toggle("hidden");
+  });
+
+const button = document.querySelector ('[data-js="answer_button"]')
+const answer = document.querySelector ('[data-js="answer"]')
+const buttonHide = document.querySelector ('[data-js="answer_button_hide"]')
+
+button.addEventListener("click", () => {
+    answer.classList.toggle("hidden");
+    button.classList.toggle("hidden");
+    buttonHide.classList.toggle("hidden");
+  });
+
+  buttonHide.addEventListener("click", () => {
+    answer.classList.toggle("hidden");
+    button.classList.toggle("hidden");
+    buttonHide.classList.toggle("hidden");
+  });
