26fd7c018ac3da6982416c12ca808a518d6ef813
Nadine Großkreuz <nadine.grosskreuz@web.de>
2022-04-01 12:54:58 +0200

    Add cards.js and split JS

--- index.js
+++ index.js
@@ -1,28 +1,6 @@
-const bookmark = document.querySelector ('[data-js="bookmark"]')
-const bookmarkActive = document.querySelector ('[data-js="bookmarkActive"]')
+import Card from "./Cards/cards.js";
+const cardElements = document.querySelectorAll('[data-js="card"]');
 
-bookmark.addEventListener("click", () => {
-    bookmark.classList.toggle("hidden");
-    bookmarkActive.classList.toggle("hidden");
-  });
-
-  bookmarkActive.addEventListener("click", () => {
-    bookmarkActive.classList.toggle("hidden");
-    bookmark.classList.toggle("hidden");
-  });
-
-const button = document.querySelector ('[data-js="answer_button"]')
-const answer = document.querySelector ('[data-js="answer"]')
-const buttonHide = document.querySelector ('[data-js="answer_button_hide"]')
-
-button.addEventListener("click", () => {
-    answer.classList.toggle("hidden");
-    button.classList.toggle("hidden");
-    buttonHide.classList.toggle("hidden");
-  });
-
-  buttonHide.addEventListener("click", () => {
-    answer.classList.toggle("hidden");
-    button.classList.toggle("hidden");
-    buttonHide.classList.toggle("hidden");
-  });
+cardElements.forEach((cardElement) => {
+  Card(cardElement);
+});
