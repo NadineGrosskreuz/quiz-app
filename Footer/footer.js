f742bbe7639f06ed134d4a25c6f9170e4f5db3c0
Nadine Großkreuz <nadine.grosskreuz@web.de>
2022-04-01 17:18:53 +0200

    Create single page application

---
+++ Footer/footer.js
@@ -0,0 +1,41 @@
+export default function Footer(footerElement, siteElement) {
+const houseIcon = footerElement.querySelector('[data-js="house_icon"]')
+const bookmarkIcon = footerElement.querySelector('[data-js="bookmark_icon"]')
+const pageIcon = footerElement.querySelector('[data-js="page_icon"]')
+const accountIcon = footerElement.querySelector('[data-js="account_icon"]')
+
+const indexSite = siteElement.querySelector('[data-js="index_site"]')
+const bookmarkSite = siteElement.querySelector('[data-js="bookmark_site"]')
+const createSite = siteElement.querySelector('[data-js="create_site"]')
+const profileSite = siteElement.querySelector('[data-js="profile_site"]')
+
+houseIcon.addEventListener("click", () => {
+  indexSite.classList.remove("hidden");
+  bookmarkSite.classList.add("hidden");
+  createSite.classList.add("hidden");
+  profileSite.classList.add("hidden");
+  });
+
+  bookmarkIcon.addEventListener("click", () => {
+    bookmarkSite.classList.remove("hidden");
+    indexSite.classList.add("hidden");
+    createSite.classList.add("hidden");
+    profileSite.classList.add("hidden");
+  });
+
+  pageIcon.addEventListener("click", () => {
+    bookmarkSite.classList.add("hidden");
+    indexSite.classList.add("hidden");
+    createSite.classList.remove("hidden");
+    profileSite.classList.add("hidden");
+  });
+
+  accountIcon.addEventListener("click", () => {
+    bookmarkSite.classList.add("hidden");
+    indexSite.classList.add("hidden");
+    createSite.classList.add("hidden");
+    profileSite.classList.remove("hidden");
+  });
+
+
+}
--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 import Card from "./Cards/cards.js";
+import Footer from "./Footer/footer.js";
+
 const cardElements = document.querySelectorAll('[data-js="card"]');
 
 cardElements.forEach((cardElement) => {
   Card(cardElement);
 });
+
+
+const footerElement = document.querySelector('[data-js="footer"]');
+const siteElement = document.querySelector('[data-js="site_elements"]');
+
+  Footer(footerElement, siteElement);
+
